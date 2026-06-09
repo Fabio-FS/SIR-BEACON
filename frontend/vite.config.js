1081742dd79119298e6a685364812521d2d2ba74
FabioSartori-88 <sartori.fabio88@gmail.com>
2026-06-09 15:23:53 +0200

    configure frontend for production deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
- 
+
 // For GitHub Pages project sites, set base to "/<repo-name>/".
 // Change this to match your repository name before deploying.
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/SIR-BEACON/",
 });
- 
